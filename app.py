d6550bda690f33639eb780b74fbc73b330949cd4
AbidiM <mohamed-abidi@live.fr>
2021-06-16 10:15:03 +0100

    f

--- app.py
+++ app.py
@@ -64,7 +64,7 @@ def invoice():
     img = Image.open(io.BytesIO(response.content))
 
     output = {}
-    pytesseract.pytesseract.tesseract_cmd = r'/app/.apt/usr/bin/tesseract'
+    pytesseract.pytesseract.tesseract_cmd = r'./.apt/usr/bin/tesseract'
     resultat = pytesseract.image_to_string(img)
     print(resultat)
     output["Adresse"] = getTextBetween(
